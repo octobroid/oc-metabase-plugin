b47136507ccab7a33b3bf47ceca4b57bdf5cdb9b
Muhammad Syehan <muhammad.syehan@gmail.com>
2022-05-10 14:15:10 +0700

    chore: make config params adjustable on ENV value

--- config/metabase.php
+++ config/metabase.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * Function to translate ENV METABASE_PARAMS from string to array
+ **/
+function translate_key_value_metabase_param($value) {
+    $value = explode('=', $value);
+    return [$value[0] => $value[1]];
+}
+
+/*
+ * You can set param on ENV within format METABASE_PARAMS=date=past26weeks,greater_than_or_equal_to=10
+ **/
+$metabase_params = array_map('translate_key_value_metabase_param', array_filter(explode(',', env('METABASE_PARAMS'))));
+
 return [
 
     /*
@@ -16,9 +29,7 @@ return [
     /*
      * Any parameters to pass to the dashboard
      **/
-    'params' => [
-        'date' => env('METABASE_PARAM_DATE', 'past26weeks')
-    ],
+    'params' => array_collapse($metabase_params),
 
     /*
      * The id of the dashboard (from the url)

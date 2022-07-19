9e614e10025fbe3738bad9b214e5e8b8234ad704
Sehan Lim <muhammad.syehan@gmail.com>
2022-07-19 16:50:20 +0700

    chore: comment config function, prevent clash config at octoberCMS v3

--- config/metabase.php
+++ config/metabase.php
@@ -3,36 +3,37 @@
 /*
  * Function to translate ENV METABASE_PARAMS from string to array
  **/
+
+/* Uncomment this function below if already copied on your own config folder **/
+
+/*
 function translate_key_value_metabase_param($value) {
     $value = explode('=', $value);
     return [$value[0] => $value[1]];
 }
+**/
 
 /*
  * You can set param on ENV within format METABASE_PARAMS="date=past26weeks,greater_than_or_equal_to=10"
  **/
-$metabase_params = array_map('translate_key_value_metabase_param', array_filter(explode(',', env('METABASE_PARAMS'))));
 
+/* Uncomment this variable if already copied on your own config folder **/
+// $metabase_params = array_map('translate_key_value_metabase_param', array_filter(explode(',', env('METABASE_PARAMS'))));
+
+/* Uncomment this return if already copied on your own config folder **/
+/*
 return [
 
-    /*
-     * The url of the metabase installation
-     **/
+     // The url of the metabase installation
     'url' => env('METABASE_URL'),
     
-
-    /* 
-     * The secret embed key from the admin settings screen
-     **/
+     // The secret embed key from the admin settings screen
     'key' => env('METABASE_KEY'),
 
-    /*
-     * Any parameters to pass to the dashboard
-     **/
+     // Any parameters to pass to the dashboard
     'params' => array_collapse($metabase_params),
 
-    /*
-     * The id of the dashboard (from the url)
-     **/
+     // The id of the dashboard (from the url)
     'dashboard_id' => (int) env('METABASE_DASHBOARD_ID')
 ];
+**/
